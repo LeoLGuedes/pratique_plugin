7090f9e7bd730af8546ce26d402088051befa774
LeoLGuedes <leonardo.guedes.08@gmail.com>
2024-02-12 23:28:00 -0300

    implantado adição visual de cadastro

--- main.js
+++ main.js
@@ -15,14 +15,46 @@ $(document).ready(function() {
         e.preventDefault()
 
         const name = $('#nome').val()
+        const cpf = $('#cpf').val()
 
         const novoItem = $('<li style="display:none;"></li>')
 
-        $(`<span>${name}</span>`).apeendTo(novoItem)
-        $(novoItem).appendTo('.lista ul')
+        $(`<span>${name}</span>`).appendTo(novoItem)
+        $(`<p>${cpf}</p>`).appendTo(novoItem)
+
+        $('.lista ul').append(novoItem)
         $(novoItem).fadeIn(1000)
 
         $('#cpf').val('')
+        $('#email').val('')
+        $('#celular').val('')
+        $('#endereço').val('')
+        $('#numeroCasa').val('')
+        $('#cep').val('')
+        $('#nome').val('')
     })
     
+    /*$('ul').on('mouseover', function(){
+
+        const name = $('#nome').val()
+        const cpf = $('#cpf').val()
+        const email = $('#email').val()
+        const cell = $('#celular').val()
+        const rua = $('#endereço').val()
+        const numRua = $('#numeroCasa').val()
+        const cep = $('#cep').val()
+
+        const dados = 
+        (`<p>Nome: ${name}</p>
+        <p>CPF: ${cpf}</p>
+        <p>Email: ${email}</p>
+        <p>Cell: ${cell}</p>
+        <p>${rua}</p>
+        <p>Numero: ${numRua}</p>
+        <p>CEP: ${cep}</p>`)
+
+        $('li')( function(){
+            
+        })
+    })*/
 })
